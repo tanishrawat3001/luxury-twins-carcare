da23c6ae36a8e52c79280994ac98c6b53f1f0585
brijeshrawat14 <rawatbrijesh1428@gmail.com>
2026-04-15 00:15:31 +0530

    Configure dynamic base for Vercel and GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Automatically use root '/' for Vercel, and '/luxury-twins-carcare/' for GitHub Pages
+  base: process.env.VERCEL ? '/' : '/luxury-twins-carcare/',
 })
